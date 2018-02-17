7863e1d40c1d0ff6c21acbe9f06e5faa2ea1b219
kievbuy <kievbuy@gmail.com>
2018-02-17 10:07:24 +0000

    create seed file with default values for 10 users and 100 posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
-100.times do |post|
-    # Post.create
+10.times do |users|
+    User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: '1122334455', password_confirmation: '1122334455')
 end
+
+100.times do |post|
+    Post.create(date: Faker::Date.backward(60), rationale: "#{post} rationale content")
+end
+
+puts "100 posts have been created"
