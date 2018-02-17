bedabdb0cfd28439deeaab2abf24e9adaaa69001
kievbuy <kievbuy@gmail.com>
2018-02-17 12:09:26 +0000

    Implemented creation process to the post and refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,35 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :delete, :edit]
     
     def index
         
+    end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(post_data)
+        
+        if @post.save
+            redirect_to post_path(@post), notice: 'Post have been sucessfully created'
+        else
+            render :new
+        end
+    end
+    
+    def show
+    end
+    
+    private
+    
+    def post_data
+        params.require(:post).permit(:date, :rationale)
+    end
+    
+    def set_post
+        @post = Post.find(params[:id])
     end
     
 end
