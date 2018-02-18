8eb9e9f361a327eb9b7b228dcb6cb04a09b45a05
kievbuy <kievbuy@gmail.com>
2018-02-18 20:16:03 +0000

    Implement posts index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
     before_action :set_post, only: [:show, :delete, :edit]
     
     def index
-        
+        @posts = Post.all
     end
     
     def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
+users = User.create(first_name: 'Kharytonenko', last_name: 'Kharytonenko', email: 'kievbuy@test.com', password: '112233', password_confirmation: '112233')
+
 10.times do |users|
     User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: '1122334455', password_confirmation: '1122334455')
 end
 
 100.times do |post|
-    Post.create(date: Faker::Date.backward(60), rationale: "#{post} rationale content")
+    Post.create(date: Faker::Date.backward(60), rationale: "#{post} rationale content", user_id: rand(1..10))
 end
 
 puts "100 posts have been created"
